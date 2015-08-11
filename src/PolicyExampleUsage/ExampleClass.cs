f320f72ea1b7176c7c8b96a427dece199e343f98
Matthias Dittrich <Matthias.Dittrich@aitgmbh.de>
2015-08-11 16:14:09 +0200

    change R# settings to match StyleCop Rules. Enable some R# hints.

--- src/PolicyExampleUsage/ExampleClass.cs
+++ src/PolicyExampleUsage/ExampleClass.cs
@@ -1,49 +1,105 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-// ReSharper disable UnusedMember.Local
+﻿// ReSharper disable UnusedMember.Local
 // ReSharper disable EventNeverSubscribedTo.Local
-#pragma warning disable 67 // Event never invoked.
-
-#pragma warning disable 169 // Unused variable
-#pragma warning disable 414 // Unused variable
 
 namespace PolicyExampleUsage
 {
+#pragma warning disable 67 // Event never invoked.
+#pragma warning disable 169 // Unused variable
+#pragma warning disable 414 // Unused variable
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
     /// <summary>
-    /// This class ensures that we can have 0 warnings -> we have no conflicts between StyleCop and ReSharper (naming style).
+    /// This class ensures that we can have 0 warnings -> we have no conflicts between StyleCop and ReSharper (naming
+    /// style).
     /// </summary>
     public class ExampleClass
     {
         /// <summary>
         /// Some Documentation.
         /// </summary>
-        public static readonly string StaticPublicReadOnlyVariable = "test";
+        public const string ConstPublicReadOnlyVariable = "test";
 
         /// <summary>
         /// Some Documentation.
         /// </summary>
-        public const string ConstPublicReadOnlyVariable = "test";
+        public static readonly string StaticPublicReadOnlyVariable = "test";
 
         /// <summary>
         /// Some Documentation
         /// </summary>
         [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Reviewed.")]
-        [SuppressMessage("Microsoft.Usage", "CA2211:NonConstantFieldsShouldNotBeVisible")]
+        [SuppressMessage("Microsoft.Usage", "CA2211:NonConstantFieldsShouldNotBeVisible", Justification = "Reviewed.")]
         public static string StaticPublicVariable = "test";
 
         /// <summary>
         /// Some Documentation.
         /// </summary>
         [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Reviewed.")]
-        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields")]
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "Reviewed.")]
         public readonly string PublicReadOnlyVariable = "test";
 
         /// <summary>
         /// Some Documentation
         /// </summary>
         [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Reviewed.")]
-        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields")]
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "Reviewed.")]
         public string PublicVariable = "test";
+
+        /// <summary>
+        /// Some Documentation.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields", Justification = "Reviewed.")]
+        private const string ConstPrivateReadOnlyVariable = "test";
+
+        /// <summary>
+        /// Some Documentation.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1802:UseLiteralsWhereAppropriate", Justification = "Reviewed.")]
+        private static readonly string StaticPrivateReadOnlyVariable = "test";
+
+        /// <summary>
+        /// Some Documentation
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields", Justification = "Reviewed.")]
+        private static string _staticPrivateVariable = "test";
+
+        /// <summary>
+        /// Some Documentation.
+        /// </summary>
+        private readonly string _privateReadOnlyVariable = "test";
+
+        /// <summary>
+        /// Some Documentation
+        /// </summary>
+        private string _privateVariable = "test";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object" /> class.
+        /// </summary>
+        public ExampleClass(string publicReadOnlyVariable, string publicVariable, string privateReadOnlyVariable, string privateVariable)
+            : this(privateReadOnlyVariable, privateVariable)
+        {
+            this.PublicReadOnlyVariable = publicReadOnlyVariable;
+            this.PublicVariable = publicVariable;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object" /> class.
+        /// </summary>
+        protected ExampleClass(string privateReadOnlyVariable)
+        {
+            this._privateReadOnlyVariable = privateReadOnlyVariable + this._privateReadOnlyVariable;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object" /> class.
+        /// </summary>
+        private ExampleClass(string privateReadOnlyVariable, string privateVariable)
+            : this(privateReadOnlyVariable)
+        {
+            this._privateVariable = privateVariable;
+        }
 
         /// <summary>
         /// Some Documentation.
@@ -53,38 +109,39 @@ namespace PolicyExampleUsage
         /// <summary>
         /// Some Documentation.
         /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1802:UseLiteralsWhereAppropriate")]
-        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private static readonly string StaticPrivateReadOnlyVariable = "test";
-
-        /// <summary>
-        /// Some Documentation.
-        /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private const string ConstPrivateReadOnlyVariable = "test";
-
-        /// <summary>
-        /// Some Documentation
-        /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private static string staticPrivateVariable = "test";
-
-        /// <summary>
-        /// Some Documentation.
-        /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private readonly string privateReadOnlyVariable = "test";
-
-        /// <summary>
-        /// Some Documentation
-        /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private string privateVariable = "test";
-
-
-        /// <summary>
-        /// Some Documentation.
-        /// </summary>
         private event EventHandler SimplePrivateEvent;
+
+        /// <summary>
+        /// Some documentation.
+        /// </summary>
+        /// <returns></returns>
+        public static string StaticPrivateMethod()
+        {
+            return StaticPrivateReadOnlyVariable;
+        }
+
+        /// <summary>
+        /// Some documentation.
+        /// </summary>
+        /// <param name="data"></param>
+        public static void StaticPublicMethod(string data)
+        {
+            StaticPublicVariable = data;
+        }
+
+        /// <summary>
+        /// Some Documentation.
+        /// </summary>
+        /// <param name="data"></param>
+        public void PublicMethod(string data)
+        {
+            this._privateVariable = data;
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Reviewed.")]
+        private string PrivateMethod()
+        {
+            return this._privateReadOnlyVariable + this._privateVariable;
+        }
     }
 }
